3fda584b8ff6d2bd9827b8f4355f9a368438c22c
Siyuan Wang <siyuanwang@ucsb.edu>
2020-01-22 17:37:28 -0800

    bug fixed

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 
 	String html = "<h1>Hello World!</h1>\n" +
 	    "<p>This web app is powered by \n" +
-	    "<a href='https://github.com/ucsb-cs56-w20/lab02-C111111'>this github repo</a></p>\n";
+	    "<a href='https://github.com/ucsb-cs56-w20/lab02-C1111111'>this github repo</a></p>\n";
 	return html;
 
     }
